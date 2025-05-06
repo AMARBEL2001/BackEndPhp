3635f5be8cddc751f03345c14bef3dc32b5d0565
AMARBEL2001 <amarbelacel16@gmail.com>
2025-05-06 17:54:14 +0100

    ok data

--- Data/One.sql
+++ Data/One.sql
@@ -1,1 +1,22 @@
-select * from One;
+create Database BACKENDPHP;
+
+use BACKENDPHP;
+
+create table One(
+    id int auto_increment primary key,
+    name varchar(50) not null,
+    age int not null,
+    email varchar(50) not null,
+    created_at datetime default current_timestamp
+);
+
+insert into
+    One(name, age, email)
+values
+    ('John Doe', 30) ; 
+
+
+select
+    *
+from
+    One;
